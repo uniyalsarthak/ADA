a2ba630ead3b483903ffb3f18ac3ebdef48ef3a3
uniyalsarthak <sarunixi@gmail.com>
2025-03-19 14:27:40 +0530

    19_march

---
+++ 19_march/activity_selection.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void sorting(vector<vector<int>> &a)
+{
+    for(int i=0;i<a.size()-1;i++)
+    {
+        int minindex=i;
+        for(int j=i+1;j<a.size();j++)
+        {
+            if(a[j][1]<a[minindex][1])
+            minindex=j;
+        }
+        if(i!=minindex)
+        swap(a[i],a[minindex]);
+    }
+}
+
+
+void activitySelection(vector<vector<int>> &activities) 
+{   //sort(activities.begin(), activities.end(), compare);  
+    sorting(activities);
+
+    for(int i=0;i<activities.size();i++)
+    {
+        cout<<"["<<activities[i][0]<<", "<<activities[i][1]<<"]"<<endl;
+    }
+
+    int lastEnd=0;  
+    cout<<"Selected activities:"<<endl;
+    for (int i=0;i<activities.size();i++) 
+    { 
+        if(activities[i][0]>=lastEnd) 
+        { 
+            cout<<"["<<activities[i][0]<<", "<<activities[i][1]<<"]"<<endl;
+            lastEnd=activities[i][1];  // Update last selected end time
+        }
+    }
+}
+
+int main() 
+{
+    vector<vector<int>> activities={{1, 3}, {2, 5}, {3, 4}, {6, 8}, {5, 7}, {9,19}, {7,10}, {12,14}};
+    activitySelection(activities);
+}
---
+++ 19_march/dijkistra.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> dijkstra(int source,vector<vector<pair<int,int>>> &graph, vector<int> &dist)
+{
+    int V=graph.size();
+    vector<bool> visited(V,false);
+    dist[source]=0;
+    for(int i=0;i<V-1;i++)
+    {
+        
+    }
+}
